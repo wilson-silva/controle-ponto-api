297866e5948a8322b9449c08ec7cece4384a2193
Wilson <wilson-gs@hotmail.com>
2021-08-24 17:50:08 -0300

    Projeto criado

---
+++ src/test/java/com/dio/live/controlepontoapi/ControlePontoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.live.controlepontoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePontoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
